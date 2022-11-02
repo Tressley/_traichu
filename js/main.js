970ddd99734384269433737d50ed164d838e8682
Tressley Cahill <tressley@gmail.com>
2022-11-02 09:36:20 -0400

    Add weather information

--- js/main.js
+++ js/main.js
@@ -13,10 +13,11 @@ function weatherBalloon(cityID) {
 			return resp.json()
 		})
 		.then(function(data) {
-	    let tempK = parseFloat(data.main.temp);
+			let weatherIcon = data.weather[0].icon;
+	    	let tempK = parseFloat(data.main.temp);
 			let tempC = Math.round(tempK - 273.15);
 			let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
-			document.getElementById('weather').innerHTML = '<p id="location">' + data.name + '</p><p id="details" ' + 'title="' + tempF + '&deg;F">' + data.weather[0].description + '<span class="separator">|</span>' + tempC + '&deg;C</p>';
+			document.getElementById('weather').innerHTML = '<p id="location">' + data.name + '</p><p id="details" ' + 'title="' + tempF + '&deg;F">' + '<img src="http://openweathermap.org/img/wn/' + weatherIcon + '@2x.png">' + data.weather[0].description + '<span class="separator">|</span>' + tempC + '&deg;C</p>';
 		});
 }
 
