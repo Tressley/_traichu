4ca2733199823f8298ab0794ee8b364adf1d7e3a
Tressley Cahill <tressley@gmail.com>
2022-11-03 11:31:16 -0400

    Update comments

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function weatherBalloon(cityID) {
 		})
 		.then(function(data) {
 			let weatherIcon = data.weather[0].icon;
-	    	let tempK = parseFloat(data.main.temp);
+			let tempK = parseFloat(data.main.temp);
 			let tempC = Math.round(tempK - 273.15);
 			let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
 			document.getElementById('weather').innerHTML = '<p id="location">' + data.name + '</p><p id="details" ' + 'title="' + tempF + '&deg;F">' + '<img src="http://openweathermap.org/img/wn/' + weatherIcon + '@2x.png">' + data.weather[0].description + '<span class="separator">|</span>' + tempC + '&deg;C</p>';
